a42bc7e356d0fa2ff084e308edb67d60a5441398
mateoveda <2214715@ucc.edu.ar>
2023-05-30 10:19:32 -0300

    Se agregaron cosas en la funcion js

---
+++ misFunciones.js
@@ -0,0 +1,10 @@
+let creartablero = () => {
+    const arreglo = [];
+    for(let f = 0;f < 10;f++){
+        arreglo.push(0);
+        for(let c = 0;c < 10;c++){
+            arrglo.push(0);
+        }
+    }
+    return arreglo;
+}
