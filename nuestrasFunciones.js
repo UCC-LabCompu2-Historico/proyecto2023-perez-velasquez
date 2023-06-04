c5ff36947c642c2e67769feb50ebfd12bf8bae9a
mateoveda <2214715@ucc.edu.ar>
2023-06-04 16:35:40 -0300

    La última actualizacion por errores de merge

--- nuestrasFunciones.js
+++ nuestrasFunciones.js
@@ -1,49 +1,19 @@
-<<<<<<< HEAD
-const tablero = document.getElementById("tablero");
-const selector = document.getElementById("tamanio");
-
-function crearTablero(tamanio) {
-    const [columnas, filas] = tamanio.split("x");
-    tablero.innerHTML = "";
-
-    const anchoCasilla = 800 / columnas;
-    const altoCasilla = 400 / filas;
-
-    for (let fila = 0; fila < filas; fila++) {
-        for (let columna = 0; columna < columnas; columna++) {
-            const casilla = document.createElement("div");
-            casilla.classList.add("casilla");
-            casilla.style.width = `${anchoCasilla}px`;
-            casilla.style.height = `${altoCasilla}px`;
-            tablero.appendChild(casilla);
-        }
-    }
-}
-
-selector.addEventListener("change", function () {
-    const tamanioSeleccionado = selector.value;
-    crearTablero(tamanioSeleccionado);
-});
-
-// Crear el tablero inicial
-crearTablero(selector.value);
-=======
 document.addEventListener("DOMContentLoaded", function() {
   const tablero = document.getElementById("tablero");
   const selectDificultad = document.getElementById("gamemode");
-  
+
   /**
-    * Crea el tablero de juego, con celdas segun la dificultad elegida
-    * @method crearTablero
-    * @param {const} columnas
-    * @param {const} filas
-    */
+   * Crea el tablero de juego, con celdas segun la dificultad elegida
+   * @method crearTablero
+   * @param {const} columnas
+   * @param {const} filas
+   */
   function crearTablero(columnas, filas) {
     tablero.innerHTML = "";
-  
+
     const anchoCasilla = 800 / columnas;
     const altoCasilla = 400 / filas;
-  
+
     for (let fila = 0; fila < filas; fila++) {
       for (let columna = 0; columna < columnas; columna++) {
         const casilla = document.createElement("div");
@@ -54,15 +24,15 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     }
   }
-  
+
   /**
-    * Selecciona la dificultad del juego y, en consecuencia, crea el
-    * tablero efectivamente llamando a la función correspondiente
-    * @method selectDificultad
-    */
+   * Selecciona la dificultad del juego y, en consecuencia, crea el
+   * tablero efectivamente llamando a la función correspondiente
+   * @method selectDificultad
+   */
   selectDificultad.addEventListener("change", function () {
     const dificultadSeleccionada = selectDificultad.value;
-  
+
     if (dificultadSeleccionada === "facil") {
       crearTablero(10, 5);
     } else if (dificultadSeleccionada === "intermedio") {
@@ -71,9 +41,8 @@ document.addEventListener("DOMContentLoaded", function() {
       crearTablero(40, 20);
     }
   });
-  
+
   //Tablero inicial
   crearTablero(10, 5);
-  
-  });
->>>>>>> main
+
+});
