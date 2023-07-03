920fa6e36f6458a56a35b72abe9ac09c321b9507
mateoveda <2214715@ucc.edu.ar>
2023-07-03 17:16:36 -0300

    Se agregaron muchas cosas para comentar en un solo commit

--- nuestrasFunciones.js
+++ nuestrasFunciones.js
@@ -8,6 +8,12 @@ document.addEventListener('DOMContentLoaded', function() {
     let comienzo = false;
     let banderas = minas;
 
+
+    /**
+     * Crea un nuevo juego, dependiendo si es necesario por un cambio de dificultad
+     * o por que simplemente necesita no caer en una casilla con mina
+     * @method nuevoJuego
+     */
     let nuevoJuego = () => {
         crearTablero();
         eventosCampo();
@@ -16,8 +22,8 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     /**
-     * Crea el tablero de juego, con celdas segun la dificultad
-     * elegida, pero en el HTML, es el tablero visible
+     * Crea el tablero de juego, con celdas segun la dificultad elegida, pero en el HTML,
+     * es el tablero visible
      * @method crearTablero
      */
     let crearTablero = () => {
@@ -36,13 +42,23 @@ document.addEventListener('DOMContentLoaded', function() {
         tableroHTML.style.height = filas * ancho + "px";
     }
 
+    /**
+     * Crea el tablero de juego, con celdas segun la dificultad elegida, pero en el Javascript,
+     * o sea es el tablero no visible.
+     * @method crearCampo
+     */
     let crearCampo = () => {
         vaciarCampo();
-        ponerBombas();
+        ponerMinas();
         valorizarCampo();
-        contarBombas();
+        contarMinas();
     }
 
+    /**
+     * Permite vaciar el campo, o sea resetear todos los valores del campo anterior,
+     * igualando el campo a un arreglo vacío y en cada arreglo vacío pushea otro arreglo vacío
+     * @method crearTablero
+     */
     let vaciarCampo = () => {
         campo = [];
         for (let c = 0; c < columnas; c++) {
@@ -50,7 +66,12 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    let ponerBombas = () => {
+    /**
+     * Crea el tablero de juego, con celdas segun la dificultad elegida, pero en el HTML,
+     * es el tablero visible
+     * @method ponerMinas
+     */
+    let ponerMinas = () => {
         for(let i = 0;i < minas;i++){
             let c;
             let f;
@@ -63,6 +84,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
     }
 
+    /**
+     * Permite poder asignarle a todas las casillas que no tengan nada asignado
+     * el valor momentaneo 0.
+     * @method valorizarCampo
+     */
     let valorizarCampo = () => {
         for (let c = 0;c < columnas;c++){
             for(let f = 0;f < filas;f++){
@@ -73,6 +99,13 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
+    /**
+     * Permite contar la cantidad de Minas que tiene una casilla a su alrededor
+     * y devuelve el número cálculado
+     * @method contar
+     * @param columna
+     * @param  fila
+     */
     let contar = (columna,fila) => {
         let resultado = 0;
         for (let x = columna - 1; x <= columna + 1; x++) {
@@ -84,7 +117,13 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         return resultado;
     }
-    let contarBombas = () => {
+
+    /**
+     * Es la función que se encarga de poder actualizar el valor de las casillas,
+     * usando la función contar, asignamos ese valor a las casillas del campo
+     * @method contarMinas
+     */
+    let contarMinas = () => {
         for (let c = 0;c < columnas;c++){
             for(let f = 0;f < filas;f++){
                 if(campo[c][f].valor !==-1){
@@ -94,6 +133,16 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
+    /**
+     * Permite verificar el estado del campo, de tal manera que lee los valores, y logra
+     * interactuar con el tablero del HTML, para que cada uno de los valores de las casillas
+     * tenga sentido a la hora de jugar, utilizando los estados "undefined", "destapado" y
+     * "bandera", en caso de ser undefined, significa que la casilla está tapada, destapado
+     * significa que está tapada, y la bandera significa que el usuario apretó para poner una
+     * bandera, y cada uno de estos estados se programó de manera que tenga sentido con el j
+     * juego clásico del buscaminas
+     * @method actualizarCampo
+     */
     let actualizarCampo = () => {
         for(let c = 0;c < columnas;c++){
             for(let f = 0;f < filas;f++){
@@ -104,6 +153,7 @@ document.addEventListener('DOMContentLoaded', function() {
                         case -1:
                             //es un favicon que usamos de la libreria font-awesome.
                             casilla.innerHTML = `<i class="fa-solid fa-land-mine-on"></i>`;
+                            casilla.style.background = "#cbc1c1";
                             break;
                         case 0:
                             break;
@@ -118,6 +168,61 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
                 if(campo[c][f].estado === undefined){
                     casilla.innerHTML = ``;
+                }
+                if(campo[c][f].estado === "destapado"){
+                    casilla.style.color = "black";
+                }
+            }
+        }
+        ganar();
+        perder();
+    }
+
+    /**
+     * Esta función se indica de ver si el jugador ha ganado, lo hace verificando que todas
+     * las casillas con valor -1 estén tapadas, en caso contrario sale de la función y se activa
+     * la funcion perder
+     * @method ganar
+     */
+    let ganar = () => {
+        for(let c = 0;c < columnas;c++) {
+            for (let f = 0; f < filas; f++) {
+                if(campo[c][f].estado !== "destapado" && campo[c][f].valor === -1){
+                    continue;
+                }else{
+                    return;
+                }
+            }
+        }
+        let tableroHTML = document.getElementById("tablero");
+        tableroHTML.style.background = "green";
+        estado = false;
+    }
+
+    /**
+     * Esta función se indica de ver si el jugador ha perdido, lo hace verificando si el
+     * usuario destapó una mina, en ese caso el tablero se pone rojito.
+     * @method perder
+     */
+    let perder = () => {
+        for(let c = 0;c < columnas;c++){
+            for(let f = 0;f < filas;f++){
+                if(campo[c][f].estado === "destapado" && campo[c][f].valor === -1){
+                    let tableroHTML = document.getElementById("tablero");
+                    tableroHTML.style.background = "#db3b26";
+                    estado = false;
+                }
+            }
+        }
+        if(estado){
+            return;
+        }
+        for(let c = 0;c < columnas;c++) {
+            for (let f = 0; f < filas; f++) {
+                if (campo[c][f].valor === -1) {
+                    let casilla = document.getElementById(`casilla-${c}-${f}`);
+                    casilla.innerHTML = `<i class="fa-solid fa-land-mine-on"></i>`;
+                    casilla.style.color = "black";
                 }
             }
         }
@@ -134,6 +239,21 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
+    let areaRecursiva = (columna,fila) => {
+        for (let x = columna - 1; x <= columna + 1; x++) {
+            for (let y = fila - 1; y <= fila + 1; y++) {
+                if(x >= 0 && y >= 0 && x < filas && y < columnas){
+                    if(campo[x][y].estado === undefined) {
+                        campo[x][y].estado = "destapado";
+                        if(campo[x][y].valor === 0){
+                            areaRecursiva(x,y)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     let clic = (casilla, c, f, me) => {
         if(!estado){
             return;
@@ -146,11 +266,14 @@ document.addEventListener('DOMContentLoaded', function() {
                 if(campo[c][f].estado === "bandera"){
                     break;
                 }
-                while(!comienzo && campo[c][f].valor === -1){
+                while(!comienzo && campo[c][f].valor !== 0){
                     nuevoJuego();
                 }
                 campo[c][f].estado = "destapado";
                 comienzo = true;
+                if(campo[c][f].valor === 0){
+                    areaRecursiva(c,f);
+                }
                 break;
             case 1: //Scroll
                 break;
